09f120785203bbccfeb5b16e858152002ece8751
Burak Yakan <burak.yakan@student.howest.be>
2026-08-06 18:20:10 +0300

    completed main functions

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ const baseCurrency = document.getElementById("base");
 const quoteCurrency = document.getElementById("quote");
 const baseAmount = document.getElementById("base-amount");
 const quoteAmount = document.getElementById("quote-amount");
+const convertButton = document.getElementById("convert-button");
+
+convertButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("Converting...");
+    document.getElementById("quote-amount").value = null;
+    getCurrencyData(baseCurrency.value, quoteCurrency.value, baseAmount.value, quoteAmount.value);
+})
 
 
-async function getCurrencyData(baseCurrency, quoteCurrency) {
+async function getCurrencyData(baseCurrency, quoteCurrency, baseAmount, quoteAmount) {
     try {
-        const currencyUrl = ``;
+        const currencyUrl = `https://api.frankfurter.dev/v2/rate/${baseCurrency}/${quoteCurrency}`;
         const currencyResponse = await fetch(currencyUrl);
         const currencyData = await currencyResponse.json();
-        
+
+
+        const {rate} = currencyData;
+
+        quoteAmount = baseAmount * rate;
+
+        console.log(`Base Currency: ${baseCurrency}, Base Amount: ${baseAmount}, Quote Currency: ${quoteCurrency}, Quote Amount: ${quoteAmount}, Exchange Rate: ${rate}`);
+
+        document.getElementById("quote-amount").value = quoteAmount;
 
     }
     catch (error) {
@@ -17,3 +33,5 @@ async function getCurrencyData(baseCurrency, quoteCurrency) {
     }
     
 }
+
+// favori donüşümler ekleme gelmeli, örneğin altta bi buton çıkmalı ona basınca direkt o dönüşüm ayarlanmalı
